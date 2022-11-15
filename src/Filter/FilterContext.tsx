257fe478267f1fef15c9de0a66004370190f86b7
gabriel-oak <gabrielcarvalhocosta@live.com>
2022-11-15 19:37:30 -0300

    checkpoint

--- src/Filter/FilterContext.tsx
+++ src/Filter/FilterContext.tsx
@@ -3,10 +3,28 @@ import React, { createContext, FC, useContext, useEffect, useState } from "react
 import { usePokeList } from "../PokeList/PokeListContex";
 import { useForm, UseFormReturn } from 'react-hook-form';
 import { Filters } from "../types/filter";
-import WorkerBuilder from "../services/worker-builder";
-import filterWorker from "../services/filter-worker";
+// import WorkerBuilder from "../services/worker-builder";
+// import filterWorker from "../services/filter-worker";
+import { Pokemon } from "../types/pokemon";
 
-const filterJob = new WorkerBuilder(filterWorker);
+const filterByName = (name: string, pokemons: Pokemon[]) => pokemons
+  .filter((pokemon) => pokemon.name.includes(name))
+
+const filterByType = (type: string, pokemons: Pokemon[]) => pokemons
+  .filter((pokemon) => pokemon.types
+    .some((t) => t.type.name.includes(type)));
+
+const filter = (filters: Filters, pokemons: Pokemon[]) => {
+  const name = filters.name.toLowerCase();
+  const type = filters.type.toLowerCase();
+  let newPokemonsToShow = Array.from(pokemons);
+  if (name) newPokemonsToShow = filterByName(name, newPokemonsToShow);
+  if (type) newPokemonsToShow = filterByType(type, newPokemonsToShow);
+
+  return newPokemonsToShow.map(({ name }) => name);
+}
+
+// const filterJob = new WorkerBuilder(filterWorker);
 
 export interface FilterContextProps {
   pokemonsToShow: string[];
@@ -23,19 +41,22 @@ export const FilterProvider: FC<{
   const form = useForm<Filters>();
   const [pokemonsToShow, setPokemonsToShow] = useState([] as string[]);
 
-  const onSubmit = form.handleSubmit((filters: Filters) => filterJob
-    .postMessage({ type: 'FILTER', filters }));
+  const onSubmit = form.handleSubmit((filters: Filters) =>
+    setPokemonsToShow(filter(filters, pokemons)));
+
+  // const onSubmit = form.handleSubmit((filters: Filters) => filterJob
+  //   .postMessage({ type: 'FILTER', filters }));
 
   useEffect(() => {
-    filterJob.onmessage = (message) => message && setPokemonsToShow(message.data);
-    filterJob.postMessage({ type: 'INIT', pokemons});
-    filterJob.postMessage({
-      type: 'FILTER',
-      filters: { name: '', type: '' },
-    });
+    // filterJob.onmessage = (message) => message && setPokemonsToShow(message.data);
+    // filterJob.postMessage({ type: 'INIT', pokemons });
+    // filterJob.postMessage({
+    //   type: 'FILTER',
+    //   filters: { name: '', type: '' },
+    // });
+    setPokemonsToShow(pokemons.map(({name}) => name));
     form.reset();
   }, [pokemons]);
-
 
   return (
     <FilterContext.Provider value={{
